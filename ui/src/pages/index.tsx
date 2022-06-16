8a46a4b69f89657fd6cb6ae9a60ba53c6eff43b4
FelipeVahia <felipevahia@gmail.com>
2022-06-15 22:31:11 -0300

    feat: implement initial configs and folders for development

--- ui/src/pages/index.tsx
+++ ui/src/pages/index.tsx
@@ -6,7 +6,7 @@ const Home: NextPage = () => {
     flex h-screen justify-center items-center
     bg-gradient-to-r from-purple-500 to-blue-600
     `}>
-      <span className='text-4xl'> Texto </span>
+      <span className='text-4xl'> MusicAll </span>
     </div>
   )
 }
